2a1e40b9e00861136c1947665dd29340a012dada
Lucialopez16 <102190572+Lucialopez16@users.noreply.github.com>
2022-03-23 20:25:27 -0300

    Add files via upload

---
+++ #ejerciocio_2 Lucia Lopez.py
@@ -0,0 +1,11 @@
+#ejerciocio_2 Lucia Lopez
+#calcular el area de un triangulo
+
+nombre = input ("ingrese su nombre: ")
+print("hola" + nombre)
+
+base = float(input("ingrese la base: "))
+altura = float(input("ingrese la altura: "))
+area= (base * altura /2)
+print ("el area del triangulo es de: ", area)
+print("suerte!")
